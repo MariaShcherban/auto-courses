ab4c223a59bdfaec17b1d956cd2a6d0c64e1a852
MariaShcherban <maria.shcherban@arcadia.spb.ru>
2022-02-23 18:13:35 +0300

    Lesson1: task one, added all three expressions

---
+++ TaskOne/ProjectOne/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ProjectOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, z, result;
+
+            Console.WriteLine("Please enter 3 numbers one by one and press Enter after every number");
+            if (double.TryParse(Console.ReadLine(), out x)) 
+            {
+                if (double.TryParse(Console.ReadLine(), out y))
+                {
+                    if (double.TryParse(Console.ReadLine(), out z))
+                    {
+                        result = (x + y) * (Math.Pow(z, 2) + 1);
+                        Console.WriteLine($"First expression result is: {result}");
+
+                        result = (x % z - 1) * Math.Sqrt(y);
+                        Console.WriteLine($"Second expression result is: {result}");
+
+                        result = (x*y + y*z) / Math.Pow(z, 3);
+                        Console.WriteLine($"Third expression result is: {result}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"The third input is not a number");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"The second input is not a number");
+                }
+            } 
+            else
+            {
+                Console.WriteLine($"The first input is not a number");
+            }
+        }
+    }
+}
