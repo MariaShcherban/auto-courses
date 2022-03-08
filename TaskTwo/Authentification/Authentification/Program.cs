3981441e5ab42fa2f4787a83c23d83df3d08d674
MariaShcherban <maria.shcherban@arcadia.spb.ru>
2022-03-08 16:39:13 +0300

    Task2: added Authentification application

---
+++ TaskTwo/Authentification/Authentification/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Authentification
+{
+    class Program
+    {
+        private const string userName = "admin";
+        private const string password = "a";
+        private const int numberOfAttempts = 3;
+
+        public bool CheckUserCredentials(string login, string pswd)
+        {
+            if (login.Equals(userName) && pswd.Equals(password))
+                return true;
+            else
+                return false;
+        }
+
+        public static void Main(string[] args)
+        {
+            Program p = new Program();
+            string usernameAttempt, passwordAttempt;
+            bool isLoginSuccessfull = false;
+
+            for (int i = 0; i < numberOfAttempts; i++)
+            {
+                Console.WriteLine("Enter username");
+                usernameAttempt = Console.ReadLine();
+                Console.WriteLine("Enter password");
+                passwordAttempt = Console.ReadLine();
+                isLoginSuccessfull = p.CheckUserCredentials(usernameAttempt, passwordAttempt);
+                if (isLoginSuccessfull)
+                {
+                    Console.WriteLine("User credentials are correct");
+                    break;
+                }
+                else
+                    Console.WriteLine("Username or password is incorrect. Please try again");
+                    
+            }
+            if (!isLoginSuccessfull)
+            {
+                Console.WriteLine("User credentials are incorrect. Press any key to close the application");
+                Console.Read();
+            }
+        }
+    }
+}
