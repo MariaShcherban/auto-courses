e98783fe015664d1b14a222d47c98efed94c7745
MariaShcherban <manya112007@gmail.com>
2022-04-21 22:01:20 +0300

    HW3: added single class

---
+++ TaskFour/Single/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using SingleInstance;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"The fist hash = {SingleClass.GetInstance().GetHashCode()}");
+            Console.WriteLine($"The second hash = {SingleClass.GetInstance().GetHashCode()}");
+        }
+    }
+}
